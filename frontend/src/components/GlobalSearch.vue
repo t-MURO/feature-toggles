f56e621051d0133b432d9d54d056b79036bb8642
Tadeo <tadeo@pop-os.localdomain>
2019-10-31 00:14:11 +0100

    added linking on enter in global search

--- frontend/src/components/GlobalSearch.vue
+++ frontend/src/components/GlobalSearch.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <v-autocomplete
     auto-select-first
     text
@@ -11,13 +11,20 @@
     item-value="_id"
   >
     <template v-slot:item="data">
-      <v-list-item-content @click="$router.push(data.item.path)">
-        <v-list-item-title>
-          {{ data.item.name }}
-          <v-chip dense>{{ data.item.type }}</v-chip>
-        </v-list-item-title>
-        <v-list-item-subtitle>{{ data.item.description }}</v-list-item-subtitle>
-      </v-list-item-content>
+      <v-list-item
+        @keydown="$router.push(data.item.path)"
+        :to="{ path: data.item.path }"
+      >
+        <v-list-item-content @click="$router.push(data.item.path)">
+          <v-list-item-title>
+            {{ data.item.name }}
+            <v-chip dense>{{ data.item.type }}</v-chip>
+          </v-list-item-title>
+          <v-list-item-subtitle>{{
+            data.item.description
+          }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
     </template>
   </v-autocomplete>
 </template>
