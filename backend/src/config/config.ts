e60997c71b8b370a99832ef77b13b798ce2446ed
Tadeo Muro <tadeomuro@gmail.com>
2020-04-22 16:19:48 +0200

    removed unused code

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,6 +1,6 @@
 export const AUTH_COOKIE_NAME = "access_token";
 
-export const API_AUTH_SECRET = "asdd9a8sf7498frfnhrt0cu04m0tv7";
+export const API_AUTH_SECRET = process.env.FTMS_SECRET || "secret";
 
 export const TOKEN_EXPIRY_TIME = 60 * 60 * 2; // 2h
 // export const TOKEN_EXPIRY_TIME = 60; // 2h
